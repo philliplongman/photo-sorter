7c08f2d746d6e749c9ea8cecfbb0963b4b6464e3
Phillip Longman <himself@philliplongman.com>
2016-10-22 21:42:25 -0400

    Undo tagging.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
 
   def edit
     @photo = Photo.find(params[:id])
-    @tag_groups = TagGroup.includes(:tags)
+    tag_groups
   end
 
   def update
@@ -12,12 +12,19 @@ class PhotosController < ApplicationController
     @photo.tagged_at = (@photo.tag_ids.empty?) ? nil : DateTime.now
 
     flash.alert = "Error" unless @photo.save
-    redirect_to random_photos_path
+    redirect_to root_path
   end
 
   def random
     @photo = Photo.where(tagged_at: nil).random_order.first
-    @tag_groups = TagGroup.includes(:tags)
+    tag_groups
+    render :edit
+  end
+
+  def undo
+    @photo = Photo.where.not(tagged_at: nil).order(:tagged_at).last
+    @photo.update(tagged_at: nil)
+    tag_groups
     render :edit
   end
 
@@ -25,6 +32,10 @@ class PhotosController < ApplicationController
 
   def tag_ids_from_params
     params[:tag_ids].select {|k, v| v == "1"}.keys
+  end
+
+  def tag_groups
+    @tag_groups = TagGroup.includes(:tags)
   end
 
 end
